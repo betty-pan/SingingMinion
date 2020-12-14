5afd80f853661e24e9fc024e88b50501263565f4
BettyPan <betty19931218@gmail.com>
2020-12-14 14:01:26 +0800

    Initial Commit

---
+++ SingingMinion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SingingMinion
+//
+//  Created by BettyPan on 2020/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
